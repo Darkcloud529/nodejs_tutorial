b1c6550c79bf38ef16027a6f47946640edf056a7
Sangmin <bbsm8671@icloud.com>
2022-03-26 23:41:38 +0900

    ExpressMiddleWare01

--- chapter6/learn-express/app.js
+++ chapter6/learn-express/app.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+
+// 작성할 때 순서가 중요합니다. 
+// 위에서부터 아래로 진행됩니다!
 app.set( 'port', process.env.PORT || 3000);
 
-app.get('/', (req,res) => {
+app.use((req,res,next) => {
+    console.log('모든 요청에 다 실행됩니다.');
+    next(); // 다음 라우터 중에 일치하는 것을 실행함.
+});
+
+// 라우터 매개 변수 설정
+// app.get('/category/:name', (req,res) => {
+//     res.send(`hello ${req.params.name}`);
+// });
+
+// 모든 get 요청 주소에 실행한다는 뜻
+// app.get('*', (req,res) => {
+//     res.send('hello everybody');
+// });
+
+app.get('/', (req,res, next) => {
     //res.send('Hello, Express');
+    console.log('GET / 요청에서만 실행됩니다.');
+    next();
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
