9e4b96dc9e566a9217fe7795044938c0d607045c
Sangmin <bbsm8671@icloud.com>
2022-04-19 23:47:01 +0900

    NodeWebAPI05

--- chapter10/nodebird-api/routes/v1.js
+++ chapter10/nodebird-api/routes/v1.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 
 const {verifyToken} = require('./middlewares');
-const {User, Domain} = require('../models');
+const {User, Domain, Post, Hashtag} = require('../models');
 
 const router = express.Router();
 
@@ -48,6 +48,49 @@ router.post('/token', async (req, res) => {
 // 사용자가 발급받은 토큰 테스트 라우터
 router.get('/test', verifyToken, (req,res) => {
     res.json(req.decoded);
+});
+
+// 내가 올린 포스터 가져오기
+router.get('/posts/my', verifyToken, (req, res) => {
+    Post.findAll({where: {userId: req.decoded.id}})
+        .then((post) => {
+            console.log(posts);
+            res.json({
+                code: 200,
+                payload: posts,
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json({
+                code: 500,
+                message: '서버 에러',
+            });
+        });
+});
+
+// 해시 태그 검색 결과 가져오기
+router.get('/posts/hashtag/:title', verifyToken, async(req, res) => {
+    try {
+        const hashtag = await Hashtag.findOne({where: {title: req.params.title}});
+        if(!hashtag) {
+            return res.status(404).json({
+                code: 404,
+                message: '검색 결과가 없습니다.',
+            });
+        }
+        const posts = await hashtag.getPosts();
+        return res.json({
+            code: 200,
+            payload: posts,
+        });
+    } catch(error) {
+        console.error(error);
+        return res.status(500).json({
+            code:500,
+            message: '서버 에러',
+        });
+    }
 });
 
 module.exports = router;
